a2dda700b2b7b0242d10b7e26b30123ba632337f
dhakarvinay <146158255+dhakarvinay@users.noreply.github.com>
2023-09-29 11:42:48 +0530

    day 2 python

---
+++ python.py
@@ -0,0 +1,13 @@
+print(100)
+print("hii there")
+#    print(10+6)
+x=100
+a=3.7
+z="hello bro"
+print(x,a,z)
+# printing data types of variables
+print(type(z))
+# maths operations
+sum1=x+a
+sum2=x-a
+print(sum1,sum2)
---
+++ task1.py
@@ -0,0 +1,7 @@
+# Ask the user to enter two numbers
+n1 = input("Enter the first number: ")
+#by default input takes all argument as string
+n2 = input("Enter the second number: ")
+#lets change data types to int (type casting)
+sum1=int(n1)+(n2)
+print(sum1)
