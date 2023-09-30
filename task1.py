f3473a5147a5ee21c0cbb9b00a5f55d0d3c14ad8
dhakarvinay <146158255+dhakarvinay@users.noreply.github.com>
2023-09-30 10:08:58 +0530

    task1.py

--- task1.py
+++ task1.py
@@ -3,5 +3,5 @@ n1 = input("Enter the first number: ")
 #by default input takes all argument as string
 n2 = input("Enter the second number: ")
 #lets change data types to int (type casting)
-sum1=int(n1)+(n2)
+sum1=int(n1)+int(n2)
 print(sum1)
